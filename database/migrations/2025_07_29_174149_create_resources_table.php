d4c3497d8ffda60151b5640a75745129d8ad3fa2
Mohimenul-Islam <islam15-5725@diu.edu.bd>
2025-07-30 00:00:24 +0600

    feat: Implement resource upload and display

---
+++ database/migrations/2025_07_29_174149_create_resources_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+   public function up(): void
+{
+    Schema::create('resources', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Links to the user who uploaded it
+        $table->string('title');
+        $table->text('description');
+        $table->string('course_code');
+        $table->string('file_path'); // This will store the location of the uploaded file
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resources');
+    }
+};
